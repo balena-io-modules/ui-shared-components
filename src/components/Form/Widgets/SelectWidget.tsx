783ac4bf9058009b73291b7a2551c20f1eab77db
Andrea Rosci <jsreds@Andreas-MBP.lan>
2023-11-10 02:53:55 +0100

    Fix Autocomplete widget selection
    Change-type: patch

--- src/components/Form/Widgets/SelectWidget.tsx
+++ src/components/Form/Widgets/SelectWidget.tsx
@@ -5,6 +5,7 @@ import {
 	InputLabel,
 } from '@mui/material';
 import { WidgetProps } from '@rjsf/utils';
+import { isEqual } from 'lodash';
 
 export const SelectWidget = ({
 	id,
@@ -18,10 +19,6 @@ export const SelectWidget = ({
 	options,
 	required,
 }: WidgetProps) => {
-	const change = ({ target: { value } }: any) => {
-		return onChange(value === '' ? options.emptyValue : value);
-	};
-
 	const { enumOptions, enumDisabled } = options;
 	if (!Array.isArray(enumOptions)) {
 		return null;
@@ -38,6 +35,20 @@ export const SelectWidget = ({
 		};
 	});
 
+	// 	This components needs to handle two cases, single selection and multiple selections.
+	// 	value, getOptionLabel, getOptionDisabled and onChange has to consider 2 scenarios following this type:
+	// 	option: {
+	// 	    disabled: boolean;
+	// 	    value: any;
+	// 	    label: string;
+	// 	    schema?: RJSFSchema | undefined;
+	// 	} | {
+	// 	    disabled: boolean;
+	// 	    value: any;
+	// 	    label: string;
+	// 	    schema?: RJSFSchema | undefined;
+	// 	}[]
+
 	return (
 		<FormControl>
 			<InputLabel htmlFor={id} required={required}>
@@ -47,7 +58,11 @@ export const SelectWidget = ({
 				fullWidth
 				id={id}
 				value={
-					value !== undefined
+					Array.isArray(value)
+						? selectOptions.filter((option) =>
+								value.some((v) => option.value === v),
+						  )
+						: value !== undefined
 						? selectOptions.find((option) => option.value === value)
 						: undefined
 				}
@@ -58,11 +73,38 @@ export const SelectWidget = ({
 				}}
 				disabled={disabled}
 				options={selectOptions}
-				isOptionEqualToValue={(option, value) => option.value === value.value}
-				getOptionLabel={(option) => option.label}
-				getOptionDisabled={(option) => option.disabled}
+				isOptionEqualToValue={(option, value) => isEqual(option, value)}
+				getOptionLabel={(option) =>
+					Array.isArray(option)
+						? option.map((o) => o.label).join(', ')
+						: option.label
+				}
+				getOptionDisabled={(option) =>
+					Array.isArray(option)
+						? option.some((o) => o.disabled)
+						: option.disabled
+				}
 				renderInput={(params) => <TextField {...params} />}
-				onChange={change}
+				onChange={(_event, selected) => {
+					if (Array.isArray(selected)) {
+						const val = selected
+							.map((item) => {
+								if (item && typeof item === 'object' && 'value' in item) {
+									return item.value;
+								}
+								// this case cover a wrong setup, we just return undefined
+								return undefined;
+							})
+							.filter((v) => v !== undefined);
+						return onChange(val.length > 0 ? val : options.emptyValue);
+					}
+					if (selected && typeof selected === 'object' && 'value' in selected) {
+						return onChange(
+							selected.value === '' ? options.emptyValue : selected.value,
+						);
+					}
+					return onChange(options.emptyValue);
+				}}
 				disableClearable
 			/>
 		</FormControl>
