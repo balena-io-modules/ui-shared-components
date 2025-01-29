e62480c1050af6c758d490202a51ae2f9322aa92
Andrea Rosci <andrea.rosci90@gmail.com>
2025-01-29 10:13:13 +0000

    Update react-query and export it
    Change-type: patch

--- src/components/RJST/Actions/Tags.tsx
+++ src/components/RJST/Actions/Tags.tsx
@@ -9,7 +9,7 @@ import type {
 	SubmitInfo,
 } from '../../TagManagementDialog/models';
 import { closeSnackbar, enqueueSnackbar } from 'notistack';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 import { Spinner } from '../../Spinner';
 import { TagManagementDialog } from '../../TagManagementDialog';
 
