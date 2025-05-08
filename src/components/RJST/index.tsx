8f20b2826e76885f926fc9037e3b166b1135fc6e
myarmolinsky <yarmolinskymatthew@gmail.com>
2025-05-08 09:50:44 -0400

    Rename Widget `extraContext` to `resource`
    Change-type: major

--- src/components/RJST/index.tsx
+++ src/components/RJST/index.tsx
@@ -838,7 +838,7 @@ const getColumnsFromSchema = <T extends RJSTBaseResource<T>>({
 							extraFormats={[...(formats ?? []), ...defaultFormats]}
 							schema={widgetSchema}
 							value={calculatedField}
-							extraContext={entry}
+							resource={entry}
 						/>
 					);
 				},
