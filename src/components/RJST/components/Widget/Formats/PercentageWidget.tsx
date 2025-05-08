e171aab42694be794a4f90dd4cac672b0d41c56e
myarmolinsky <yarmolinskymatthew@gmail.com>
2025-05-08 11:59:37 -0400

    widgetFactory: Remove `extraProps` parameter and generic typing
    Change-type: major

--- src/components/RJST/components/Widget/Formats/PercentageWidget.tsx
+++ src/components/RJST/components/Widget/Formats/PercentageWidget.tsx
@@ -1,6 +1,6 @@
 import { JsonTypes, widgetFactory } from '../utils';
 
-export const PercentageWidget = widgetFactory('Percentage', {}, [
+export const PercentageWidget = widgetFactory('Percentage', [
 	JsonTypes.string,
 	JsonTypes.number,
 ])(({ value }) => {
