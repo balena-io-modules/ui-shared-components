e51c7acea7f176507e84f3ba61e274718f822e05
myarmolinsky <yarmolinskymatthew@gmail.com>
2025-05-16 10:02:10 -0400

    Drop deprecated interface `OsSpecificDeviceTypeJsonInstructions`
    Change-type: major

--- src/components/DownloadImageDialog/ApplicationInstructions.tsx
+++ src/components/DownloadImageDialog/ApplicationInstructions.tsx
@@ -1,13 +1,9 @@
 import has from 'lodash/has';
 import { interpolateMustache } from './utils';
 import { Box, List, Tab, Tabs, Typography } from '@mui/material';
-import { memo, useEffect, useMemo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { MUILinkWithTracking } from '../MUILinkWithTracking';
-import type {
-	DeviceType,
-	KeysOfUnion,
-	OsSpecificContractInstructions,
-} from './models';
+import type { DeviceType, OsSpecificContractInstructions } from './models';
 import { OrderedListItem } from '../OrderedListItem';
 import { Markdown } from '../Markdown';
 
@@ -30,12 +26,6 @@ export const getUserOs = () => {
 	return 'Unknown';
 };
 
-const dtJsonTocontractOsKeyMap = {
-	windows: 'Windows',
-	osx: 'MacOS',
-	linux: 'Linux',
-} as const;
-
 export const ApplicationInstructions = memo(function ApplicationInstructions({
 	deviceType,
 	templateData,
@@ -45,46 +35,24 @@ export const ApplicationInstructions = memo(function ApplicationInstructions({
 }) {
 	const [currentOs, setCurrentOs] = useState<OsOptions>(getUserOs());
 
-	const instructions = useMemo(() => {
-		if (
-			deviceType?.instructions == null ||
-			Array.isArray(deviceType.instructions) ||
-			typeof deviceType.instructions !== 'object'
-		) {
-			return deviceType?.instructions;
-		}
-
-		const instructionsByOs = deviceType.instructions;
-
-		return Object.fromEntries(
-			(
-				Object.entries(instructionsByOs) as Array<
-					[KeysOfUnion<typeof instructionsByOs>, string[]]
-				>
-			).map(([key, value]) => {
-				const normalizedKey =
-					key in dtJsonTocontractOsKeyMap
-						? dtJsonTocontractOsKeyMap[
-								key as keyof typeof dtJsonTocontractOsKeyMap
-							]
-						: (key as keyof OsSpecificContractInstructions);
-				return [normalizedKey, value];
-			}),
-		) as OsSpecificContractInstructions;
-	}, [deviceType?.instructions]);
-	const hasOsSpecificInstructions = !Array.isArray(instructions);
+	const hasOsSpecificInstructions = !Array.isArray(deviceType.instructions);
 	const normalizedOs = currentOs === 'Unknown' ? 'Linux' : currentOs;
 
 	useEffect(() => {
-		if (hasOsSpecificInstructions && instructions) {
-			const oses = Object.keys(instructions) as unknown as OsOptions;
+		if (hasOsSpecificInstructions && deviceType.instructions) {
+			const oses = Object.keys(deviceType.instructions);
 			if (!oses.includes(currentOs) && oses.length > 0) {
 				setCurrentOs(oses[0] as OsOptions);
 			}
 		}
-	}, [currentOs, setCurrentOs, instructions, hasOsSpecificInstructions]);
+	}, [
+		currentOs,
+		setCurrentOs,
+		deviceType.instructions,
+		hasOsSpecificInstructions,
+	]);
 
-	if (!deviceType || !instructions) {
+	if (!deviceType.instructions) {
 		return (
 			<Typography variant="body1">
 				Instructions for this device type are not currently available. Please
@@ -94,7 +62,11 @@ export const ApplicationInstructions = memo(function ApplicationInstructions({
 	}
 
 	const interpolatedInstructions = (
-		hasOsSpecificInstructions ? instructions[normalizedOs] : instructions
+		hasOsSpecificInstructions
+			? (deviceType.instructions as OsSpecificContractInstructions)[
+					normalizedOs
+				]
+			: (deviceType.instructions as string[])
 	)?.map((instruction: string) =>
 		interpolateMustache(
 			templateData,
@@ -116,15 +88,19 @@ export const ApplicationInstructions = memo(function ApplicationInstructions({
 				<Box mb={3}>
 					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
 						<Tabs
-							value={Object.keys(instructions).find((key) => key === currentOs)}
+							value={Object.keys(deviceType.instructions).find(
+								(key) => key === currentOs,
+							)}
 							onChange={(_event, value) => {
 								setCurrentOs(value ?? 'Unknown');
 							}}
 							aria-label="os tabs"
 						>
-							{(Object.keys(instructions) as OsOptions[]).map((os) => {
-								return <Tab key={os} label={os} value={os} />;
-							})}
+							{(Object.keys(deviceType.instructions) as OsOptions[]).map(
+								(os) => {
+									return <Tab key={os} label={os} value={os} />;
+								},
+							)}
 						</Tabs>
 					</Box>
 				</Box>
