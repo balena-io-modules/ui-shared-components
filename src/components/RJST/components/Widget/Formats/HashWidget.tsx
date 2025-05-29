df74d24293ca3d8d228bcbc30fa5e0a01bea2914
Andrea Rosci <andrea.rosci90@gmail.com>
2025-05-29 14:32:20 +0200

    Remove unused icon
    Change-type: patch

--- src/components/RJST/components/Widget/Formats/HashWidget.tsx
+++ src/components/RJST/components/Widget/Formats/HashWidget.tsx
@@ -1,13 +1,8 @@
-import { Code } from '@mui/icons-material';
 import { Copy } from '../../../../Copy';
 import { JsonTypes, truncateHash, widgetFactory } from '../utils';
 
 export const HashWidget = widgetFactory('Hash', {}, [JsonTypes.string])(({
 	value,
 }) => {
-	return (
-		<Copy copy={value}>
-			<Code>{truncateHash(value)}</Code>
-		</Copy>
-	);
+	return <Copy copy={value}>{truncateHash(value)}</Copy>;
 });
