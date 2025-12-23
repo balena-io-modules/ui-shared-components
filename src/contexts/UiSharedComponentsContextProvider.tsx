27f23d315b7cc4e97f12eeeab5a13c350d8e952f
Andrea Rosci <andrea.rosci90@gmail.com>
2025-12-23 13:16:35 +0100

    remove formats prop and consolidate under context
    Change-type: minor

--- src/contexts/UiSharedComponentsContextProvider.tsx
+++ src/contexts/UiSharedComponentsContextProvider.tsx
@@ -1,10 +1,12 @@
 import { createContext } from 'react';
 import type { TFunction } from '../hooks/useTranslations';
 import type { NavigateFunction } from 'react-router-dom';
+import type { Format } from '../components/RJST/components/Widget/utils';
 export interface UiSharedComponentsContextProviderInterface {
 	navigate?: NavigateFunction;
 	t?: TFunction;
 	externalTranslationMap?: Dictionary<string>;
+	RJSTFormats?: Format[];
 }
 
 export const UiSharedComponentsContextProvider =
--- src/hooks/useNavigate.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { UiSharedComponentsContextProvider } from '../contexts/UiSharedComponentsContextProvider';
-
-export const useNavigate = () => {
-	const { navigate } = React.useContext(UiSharedComponentsContextProvider);
-	return navigate;
-};
